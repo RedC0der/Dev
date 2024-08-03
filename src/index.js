b0bc87582dede3dbee147e256ba045d3085e0228
RedC0der <diwakarmishra2626@gmail.com>
2024-08-03 22:23:26 +0530

    all setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
-// import {app} from './app.js'
+import connectDB from "./db/index.js"
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
@@ -19,3 +19,21 @@ connectDB()
 
 
 
+
+// import dotenv from "dotenv"; // Import dotenv to manage environment variables
+// import connectDB from "./db/index.js"; // Import the function to connect to MongoDB
+// import { app } from './app.js'; // Import the Express app
+
+// dotenv.config({ path: './.env' }); // Configure dotenv to load variables from .env file
+
+// connectDB()
+//     .then(() => {
+//         // If the DB connection is successful, start the server
+//         app.listen(process.env.PORT || 8000, () => {
+//             console.log(`⚙️ Server is running at port: ${process.env.PORT || 8000}`);
+//         });
+//     })
+//     .catch((err) => {
+//         // If the DB connection fails, log the error
+//         console.log("MONGO db connection failed !!! ", err);
+//     });
